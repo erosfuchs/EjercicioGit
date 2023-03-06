98e2ee7fbf6793c136fa2474054c073075906f3b
Eros Fuchs <erosfuchs35@gmail.com>
2023-03-06 17:32:37 -0300

    agregando ejercicios de la clase 1

---
+++ Clase 1/MyProgram.java
@@ -0,0 +1,133 @@
+public class MyProgram {
+    public static void main(String[] args) {
+
+
+         /*  1. Vamos a practicar operaciones básicas con números
+        a. Utilizando la sentencia while, imprima todos los números entre 2 variables “a”
+        y “b”. Su código puede arrancar (por ejemplo):
+        int numeroInicio = 5;
+        int numeroFin = 14;
+        // Se deberían mostrar los números:
+        5,6,7,8,9,10,11,12,13,14
+        b. A lo anterior, solo muestre los números pares
+        c. A lo anterior, con una variable extra, elija si se deben mostrar los números
+        pares o impares
+        d. Utilizando la sentencia for, hacer lo mismo que en (b) pero invirtiendo el orden */
+
+
+        /*int numeroInicio = 5;
+        int numeroFin = 14;  */
+        
+        /*""B""_)
+        
+        int numeroActual = numeroInicio;
+        while (numeroActual <= numeroFin) {
+            if (numeroActual % 2 == 0) { 
+                System.out.println(numeroActual);
+            }
+            numeroActual++;
+        } */
+
+        /*""C""_)
+
+        int numeroActual = numeroInicio;
+        while(numeroActual<= numeroFin) {
+            if(numeroActual %2 == 1) {
+                System.out.println(numeroActual);
+            }
+            numeroActual++;
+        } */
+
+        /*""D""_)
+        /* for (int i = numeroInicio; i <= numeroFin; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        } */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*""""""""""""EJERCICIO 2"""""""""""" */
+
+        /*pueden cambiar estas variables para ir probando*/
+        /* 
+        float ingresos = 800000f;
+        int vehiculos = 4;
+        int inmuebles = 5;
+        int activosdelujo = 5;
+        
+        if (ingresos >= 489.083f &&
+            vehiculos >= 3 &&
+            inmuebles >= 3 &&
+            activosdelujo >= 1) {
+            System.out.println("PERTENECE A INGRESOS ALTOS");
+        } else if (ingresos < 489.083f ||
+                   vehiculos < 3 ||
+                   inmuebles < 3 ||
+                   activosdelujo < 1) {
+            System.out.println("NO PERTENECE A INGRESOS ALTOS");
+        } */
+
+        int i = 6;
+
+/*while(i > 0) {
+
+if (i < 3) {
+
+i = i + 1;
+
+}
+
+i = i - 2;
+
+System.out.println(i);
+
+} */
+
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
+
+
+
+
